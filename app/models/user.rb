c1c6a5fc59db0b96396b81e75eb15b11f9b9bf00
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-05-12 15:00:44 -0500

    Generate devise model for user and add authenticate user method to application controller.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   belongs_to :person, polymorphic: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
