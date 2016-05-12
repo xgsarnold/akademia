0ad1832608dff3fff41b8e257977d9962f203c47
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-05-12 02:21:37 -0500

    Refactor dashboard controller and views with partials.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,13 @@
 class DashboardController < ApplicationController
   def index
-    @user = User.find_by(id: params[:id])
-    if @user.person_type == "Student"
-      @student = Student.find_by(id: @user.person_id)
+    user = User.find_by(id: params[:id])
+    if user.person_type == "Student"
+      @student = Student.find_by(id: user.person_id)
+      # @student = Student.find_by(id: 1)
       @courses = @student.my_courses
-      @classmates = 
+      # @classmates = @student.classmates()
+    else
+      @teacher = Teacher.find_by(id: user.person_id)
+      @courses = @teacher.courses
   end
 end
